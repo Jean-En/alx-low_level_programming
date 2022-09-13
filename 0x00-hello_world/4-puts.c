f29dd14f78bd24d879e6629ab5fe5d446c78fc19
Jen-En <tetchoeno@gmail.com>
2022-09-12 20:33:51 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * always: return 0 (Success)
  *
- * pintf is not allowed
+ * printf is not allowed
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
