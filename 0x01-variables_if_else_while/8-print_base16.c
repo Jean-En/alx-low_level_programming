c521b4d89a9b11204e1cc8d5fa494849be1d0310
Jen-En <tetchoeno@gmail.com>
2022-09-22 09:51:16 -0700

    betty coding style fixed

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main - only main fonction
+ *
  * Description: this program prints all the numbers of base 16
  * in lowercase, followed by a new line
+ *
  * Return: Always return 0
  */
 
