e14169d052c1201eda8f4909b5e8fb39776b036c
Jen-En <tetchoeno@gmail.com>
2022-09-22 20:22:48 -0700

    done

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,13 @@
 
 char *string_toupper(char *str)
 {
-	int i;
+	char i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] -= 32;
+			str[i] -= 'A';
 		}
 	}
 	return (str);
