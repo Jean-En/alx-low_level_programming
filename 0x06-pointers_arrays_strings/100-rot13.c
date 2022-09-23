7a7bb40b8b0b4598f069a13d07674cfea41c2a39
Jen-En <tetchoeno@gmail.com>
2022-09-22 21:19:58 -0700

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,12 +12,14 @@ char *rot13(char *s)
 {
 	char *c;
 	char rot;
-	
+
 	for (c = s; *c; ++c)
 	{
 		rot = 'A' + (*c & 32);
 		if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
+		{
 			*c = (*c - rot + 13) % 26 + rot;
+		}
 	}
 	return (s);
 }
