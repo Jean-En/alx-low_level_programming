2e578da871a3db4d43db31e82de6ab323ac63e2f
Jean-En <tetchoeno@gmail.com>
2022-10-03 12:07:37 +0000

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,18 @@
+#include "main.h"
 #include "stdio.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 98;
+	int *p;
+
+	*p = &n;
+	printf("the address of n is: %x\n",&n);
+	printf("the address of p is: %d\n",p);
+	printf("value of *p is:%d\n",*p);
+	return (0);
+}
