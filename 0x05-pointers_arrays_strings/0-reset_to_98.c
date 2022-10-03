2b48fc42e18dea45d57ba769a46b215a76b20791
Jean-En <tetchoeno@gmail.com>
2022-10-03 13:26:29 +0000

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -14,8 +14,5 @@ void reset_to_98(int *n);
 	int *p;
 
 	*p = &n;
-	printf("the address of n is: %x\n",&n);
-	printf("the address of p is: %d\n",p);
 	printf("value of *p is:%d\n",*p);
-	return (0);
 }
