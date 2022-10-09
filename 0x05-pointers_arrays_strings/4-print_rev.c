ad615a35363ef2a6f017ec2db4e64ec1d6b1202c
Jean-En <tetchoeno@gmail.com>
2022-10-09 11:28:07 +0000

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_rev - prints a string
+ * @s: parameter
+ * Description: prints a sprint in reverse followed by a new line
+ * Return: Always (0)
+ */
+
+void print_rev(char *s)
+{
+	print_rev("%s\n:",s);
+}
