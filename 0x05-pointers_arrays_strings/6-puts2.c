356fe3316df5dda48c2dae78c5ecd02772f49359
Jean-En <tetchoeno@gmail.com>
2022-10-12 15:16:45 +0000

    cool

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character starting with the fist
+ * character, followed by a new line.
+ * @str: the string to manipulate.
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
